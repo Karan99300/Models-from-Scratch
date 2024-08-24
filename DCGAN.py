359f8a812b244de948dd1977de62fc6a1b4cced5
KaranJain5690 <karanjainabhay@gmail.com>
2024-08-25 00:05:05 +0530

    Updates 25/08/24

--- DCGAN.py
+++ DCGAN.py
@@ -89,18 +89,17 @@ for epoch in tqdm(range(epochs), desc='Epochs'):
         noise=torch.randn(b_size,z_dim,1,1).to(device)
         
         fake = gen(noise)
-        disc_real = disc(real).view(-1)
-        lossD_real = criterion(disc_real, torch.ones_like(disc_real))
-        disc_fake = disc(fake).view(-1)
-        lossD_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
-        lossD = (lossD_real + lossD_fake) / 2
+        disc_real = disc(real).reshape(-1)
+        loss_disc_real = criterion(disc_real, torch.ones_like(disc_real))
+        disc_fake = disc(fake.detach()).reshape(-1)
+        loss_disc_fake = criterion(disc_fake, torch.zeros_like(disc_fake))
+        loss_disc = (loss_disc_real + loss_disc_fake) / 2
         disc.zero_grad()
-        lossD.backward()
+        loss_disc.backward()
         opt_disc.step()
         
-        output = disc(fake).view(-1)
-        lossG = criterion(output, torch.ones_like(output))
+        output = disc(fake).reshape(-1)
+        loss_gen = criterion(output, torch.ones_like(output))
         gen.zero_grad()
-        lossG.backward()
+        loss_gen.backward()
         opt_gen.step()
-        
